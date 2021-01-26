737bc08e017bf84ca3caeb9692d190716176e871
FerRom1979 <fernando_0135@hotmail.com>
2021-01-26 16:21:30 -0300

    test

--- src/components/responsive/index.tsx
+++ src/components/responsive/index.tsx
@@ -4,8 +4,13 @@ import Logo from '../../assets/img/logo.jpg';
 import Header from '../header/index';
 import Navigation from '../navigation/index';
 import * as style from './style';
+import { useHistory } from 'react-router-dom';
 
 const index = () => {
+    const history = useHistory();
+    setTimeout(() => {
+        history.push('/dashboard');
+    }, 1000);
     const { ImgLogo, LogoDiv, DivHeader, PLogo, NavDiv, DivWrapper } = style;
     return (
         <DivWrapper>
